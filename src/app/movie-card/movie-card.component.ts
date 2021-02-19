eb85e6f2810bd53ceeab978179edf403b98154c3
Lmibrahim <lameesibrahim89@gmail.com>
2021-02-20 02:10:13 +0400

    Getting the basic functionality

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { MovieWatchlistService } from '../movie-watchlist.service';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.css']
+  styleUrls: ['./movie-card.component.css'],
 })
 export class MovieCardComponent implements OnInit {
+  @Output() watchlistEvent = new EventEmitter<any>();
+  @Input() movieRef: any;
+  @Input() watchlistRef!: any[];
 
-  constructor() { }
+  watchlist: any[] = [];
 
-  ngOnInit(): void {
-  }
+  constructor(private movieService: MovieWatchlistService) {}
 
+  ngOnInit(): void {}
+
+  emitwatchlistEvent = (movie: any): void => {
+    this.watchlistEvent.emit(movie);
+  };
+
+  checkwatchList = (movie: any): boolean => {
+    return this.movieRef.some((item) => {
+      return item.id === movie.id;
+    });
+  };
 }
--- src/app/search-all/search-all.component.ts
+++ src/app/search-all/search-all.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-search-all',
   templateUrl: './search-all.component.html',
-  styleUrls: ['./search-all.component.css']
+  styleUrls: ['./search-all.component.css'],
 })
 export class SearchAllComponent implements OnInit {
+  @Output() searchEvent = new EventEmitter<string>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  emitSearchTerm = (form: NgForm) => {
+    console.log(form);
+    this.searchEvent.emit(form.form.value.searchTerm);
+  };
 }
